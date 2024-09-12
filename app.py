1cb49bf47a1a532007d807ceb870fa504c0fce46
natalieybl <94332856+natalieybl@users.noreply.github.com>
2024-09-12 18:11:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/main',methods=["get","post"])
+def main():
+    r = request.form.get("q")
+    return render_template('main.html', r = r)
+
+@app.route('/president',methods=["get","post"])
+def transfer_money():
+    return render_template('indexPresident.html')
+
+@app.route('/city',methods=["get","post"])
+def loop():
+    return render_template('indexCity.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
